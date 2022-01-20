c3df093fcd6040d93a618d328c9ceaf403cedfb6
Varshil Anavadia <vanavadia@gmail.com>
2022-01-19 21:27:20 -0800

    Added - Different Binary Search Techniques 1. Classic Binary Search 2. Find first index of target 3. Find last index of target 4. Find ceil of target (number greater than equal to) 5. Find floor of target (number less than equal to)

--- Structures/CommonAlgorithms.java
+++ Structures/CommonAlgorithms.java
@@ -4,6 +4,90 @@ import java.util.List;
 import java.util.Queue;
 
 public class CommonAlgorithms {
+
+    /*
+    * DIFFERENT BINARY SEARCH
+    *   1. CLASSIC BINARY SEARCH
+    *   2. FIND FIRST INDEX OF TARGET
+    *   3. FIND LAST INDEX OF TARGET
+    *   4. FIND CEIL OF TARGET
+    *   5. FIND FLOOR OF TARGET
+    * */
+    public int binarySearch(int[] array, int target){
+        int left = 0, right = array.length - 1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(array[mid] == target){
+                return mid;
+            } else if(array[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public int firstIndex(int[] array, int target){
+        int left = 0, right = array.length - 1, result = -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(array[mid] == target){
+                result = mid;
+                right = mid - 1;
+            } else if(array[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public int lastIndex(int[] array, int target){
+        int left = 0, right = array.length - 1, result = -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(array[mid] == target){
+                result = mid;
+                left = mid + 1;
+            } else if(array[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public int ceil(int[] array, int target){
+        int left = 0, right = array.length - 1, result = -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(array[mid] >= target){
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    public int floor(int[] array, int target){
+        int left = 0, right = array.length - 1, result = -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(array[mid] <= target){
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
     /*
     * LEVEL ORDER TRAVERSAL OF BINARY TREE - BREADTH FIRST SEARCH
     * */
